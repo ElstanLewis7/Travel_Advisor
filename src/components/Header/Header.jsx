8f29ca77eb64b8cbae31e14755150ab78e08d702
Elstan Lewis <elstanlewis7@gmail.com>
2022-04-21 20:42:46 -0400

    Working on map

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
-            Explore New Places
+            Explore New Places Here
           </Typography>
           {/* <Autocomplete> */}
           <div className={classes.search}>
