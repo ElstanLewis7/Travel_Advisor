9ca954bcb30dffc3487272a378d6eb0861d8484e
Elstan Lewis <elstanlewis7@gmail.com>
2022-04-23 21:59:50 -0400

    Added weather Api

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,45 @@
 import axios from 'axios';
 
-const URL =
-  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
-
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-    'X-RapidAPI-Key': '3a72955dd7msh15a2757f0134ffdp1b5336jsnbdd8666e8fd9',
-  },
-};
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, options);
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+          'X-RapidAPI-Key':
+            '3a72955dd7msh15a2757f0134ffdp1b5336jsnbdd8666e8fd9',
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get(
+      'https://community-open-weather-map.p.rapidapi.com/find',
+      {
+        params: { lon: lng, lat: lat },
+        headers: {
+          'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+          'X-RapidAPI-Key':
+            '3a72955dd7msh15a2757f0134ffdp1b5336jsnbdd8666e8fd9',
+        },
+      }
+    );
 
     return data;
   } catch (error) {
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
-
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+import mapStyles from './mapStyles';
+const Map = ({
+  setCoordinates,
+  setBounds,
+  coordinates,
+  places,
+  setChildClicked,
+  weatherData,
+}) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px');
 
@@ -18,13 +25,61 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={''}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
         onChange={(e) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
-        onChildClick={''}
-      ></GoogleMapReact>
+        onChildClick={(child) => {
+          setChildClicked(child);
+        }}
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isMobile ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : 'https://images.pexels.com/photos/1307698/pexels-photo-1307698.jpeg?auto=compress&cs=tinysrgb&w=300'
+                  }
+                  alt={place.name}
+                />
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+        {weatherData?.list?.map((data, i) => (
+          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+            <img
+              height={100}
+              src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+              alt={''}
+            />
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
